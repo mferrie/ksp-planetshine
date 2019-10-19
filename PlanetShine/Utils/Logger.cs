cb4a850ea23ccc5ac1691da4ac25275888b1027d
Jacob Preston <prestja@gmail.com>
2019-10-19 12:43:53 -0500

    Reformatted source files and updated changelog

--- PlanetShine/Utils/Logger.cs
+++ PlanetShine/Utils/Logger.cs
@@ -10,208 +10,164 @@ using UnityEngine;
 
 #endregion
 
-namespace PlanetShine
-{
-    [KSPAddon(KSPAddon.Startup.Instantly, false)]
-    public class Logger : MonoBehaviour
-    {
-        #region Constants
+namespace PlanetShine {
+	[KSPAddon(KSPAddon.Startup.Instantly, false)]
+	public class Logger : MonoBehaviour {
+		#region Constants
 
-        private static readonly string fileName;
-        private static readonly AssemblyName assemblyName;
+		private static readonly string fileName;
+		private static readonly AssemblyName assemblyName;
 
-        #endregion
+		#endregion
 
-        #region Fields
+		#region Fields
 
-        private static readonly List<string[]> messages = new List<string[]>();
+		private static readonly List<string[]> messages = new List<string[]>();
 
-        #endregion
+		#endregion
 
-        #region Initialisation
+		#region Initialisation
 
-        static Logger()
-        {
-            assemblyName = Assembly.GetExecutingAssembly().GetName();
-            fileName = Path.ChangeExtension(Assembly.GetExecutingAssembly().Location, "log");
-            File.Delete(fileName);
+		static Logger() {
+			assemblyName = Assembly.GetExecutingAssembly().GetName();
+			fileName = Path.ChangeExtension(Assembly.GetExecutingAssembly().Location, "log");
+			File.Delete(fileName);
 
-            lock (messages)
-            {
-                messages.Add(new[] { "Executing: " + assemblyName.Name + " - " + assemblyName.Version });
-                messages.Add(new[] { "Assembly: " + Assembly.GetExecutingAssembly().Location });
-            }
-            Blank();
-        }
+			lock (messages) {
+				messages.Add(new[] { "Executing: " + assemblyName.Name + " - " + assemblyName.Version });
+				messages.Add(new[] { "Assembly: " + Assembly.GetExecutingAssembly().Location });
+			}
+			Blank();
+		}
 
-        private void Awake()
-        {
-            DontDestroyOnLoad(this);
-        }
+		private void Awake() {
+			DontDestroyOnLoad(this);
+		}
 
-        #endregion
+		#endregion
 
-        #region Printing
+		#region Printing
 
-        public static void Blank()
-        {
-            lock (messages)
-            {
-                messages.Add(new string[] { });
-            }
-        }
+		public static void Blank() {
+			lock (messages) {
+				messages.Add(new string[] { });
+			}
+		}
 
-        public static void Log(object obj)
-        {
-            lock (messages)
-            {
-                try
-                {
-                    if (obj is IEnumerable)
-                    {
-                        messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
-                        foreach (var o in obj as IEnumerable)
-                        {
-                            messages.Add(new[] { "\t", o.ToString() });
-                        }
-                    }
-                    else
-                    {
-                        messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Exception(ex);
-                }
-            }
-        }
+		public static void Log(object obj) {
+			lock (messages) {
+				try {
+					if (obj is IEnumerable) {
+						messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
+						foreach (var o in obj as IEnumerable) {
+							messages.Add(new[] { "\t", o.ToString() });
+						}
+					} else {
+						messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
+					}
+				}
+				catch (Exception ex) {
+					Exception(ex);
+				}
+			}
+		}
 
-        public static void Log(string name, object obj)
-        {
-            lock (messages)
-            {
-                try
-                {
-                    if (obj is IEnumerable)
-                    {
-                        messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, name });
-                        foreach (var o in obj as IEnumerable)
-                        {
-                            messages.Add(new[] { "\t", o.ToString() });
-                        }
-                    }
-                    else
-                    {
-                        messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
-                    }
-                }
-                catch (Exception ex)
-                {
-                    Exception(ex);
-                }
-            }
-        }
+		public static void Log(string name, object obj) {
+			lock (messages) {
+				try {
+					if (obj is IEnumerable) {
+						messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, name });
+						foreach (var o in obj as IEnumerable) {
+							messages.Add(new[] { "\t", o.ToString() });
+						}
+					} else {
+						messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, obj.ToString() });
+					}
+				}
+				catch (Exception ex) {
+					Exception(ex);
+				}
+			}
+		}
 
-        public static void Log(string message)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, message });
-            }
-        }
+		public static void Log(string message) {
+			lock (messages) {
+				messages.Add(new[] { "Log " + DateTime.Now.TimeOfDay, message });
+			}
+		}
 
-        [System.Diagnostics.Conditional("DEBUG")]
-        [System.Diagnostics.DebuggerStepThrough]
-        public static void Debug(string message)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Debug " + DateTime.Now.TimeOfDay, message });
-            }
-        }
+		[System.Diagnostics.Conditional("DEBUG")]
+		[System.Diagnostics.DebuggerStepThrough]
+		public static void Debug(string message) {
+			lock (messages) {
+				messages.Add(new[] { "Debug " + DateTime.Now.TimeOfDay, message });
+			}
+		}
 
-        public static void Warning(string message)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Warning " + DateTime.Now.TimeOfDay, message });
-            }
-        }
+		public static void Warning(string message) {
+			lock (messages) {
+				messages.Add(new[] { "Warning " + DateTime.Now.TimeOfDay, message });
+			}
+		}
 
-        public static void Error(string message)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Error " + DateTime.Now.TimeOfDay, message });
-            }
-        }
+		public static void Error(string message) {
+			lock (messages) {
+				messages.Add(new[] { "Error " + DateTime.Now.TimeOfDay, message });
+			}
+		}
 
-        public static void Exception(Exception ex)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Exception " + DateTime.Now.TimeOfDay, ex.Message });
-                messages.Add(new[] { string.Empty, ex.StackTrace });
-                Blank();
-            }
-        }
+		public static void Exception(Exception ex) {
+			lock (messages) {
+				messages.Add(new[] { "Exception " + DateTime.Now.TimeOfDay, ex.Message });
+				messages.Add(new[] { string.Empty, ex.StackTrace });
+				Blank();
+			}
+		}
 
-        public static void Exception(Exception ex, string location)
-        {
-            lock (messages)
-            {
-                messages.Add(new[] { "Exception " + DateTime.Now.TimeOfDay, location + " // " + ex.Message });
-                messages.Add(new[] { string.Empty, ex.StackTrace });
-                Blank();
-            }
-        }
+		public static void Exception(Exception ex, string location) {
+			lock (messages) {
+				messages.Add(new[] { "Exception " + DateTime.Now.TimeOfDay, location + " // " + ex.Message });
+				messages.Add(new[] { string.Empty, ex.StackTrace });
+				Blank();
+			}
+		}
 
-        #endregion
+		#endregion
 
-        #region Flushing
+		#region Flushing
 
-        public static void Flush()
-        {
-            lock (messages)
-            {
-                if (messages.Count > 0)
-                {
-                    using (var file = File.AppendText(fileName))
-                    {
-                        foreach (var message in messages)
-                        {
-                            file.WriteLine(message.Length > 0 ? message.Length > 1 ? "[" + message[0] + "]: " + message[1] : message[0] : string.Empty);
-                            if (message.Length > 0)
-                            {
-                                print(message.Length > 1 ? assemblyName.Name + " -> " + message[1] : assemblyName.Name + " -> " + message[0]);
-                            }
-                        }
-                    }
-                    messages.Clear();
-                }
-            }
-        }
+		public static void Flush() {
+			lock (messages) {
+				if (messages.Count > 0) {
+					using (var file = File.AppendText(fileName)) {
+						foreach (var message in messages) {
+							file.WriteLine(message.Length > 0 ? message.Length > 1 ? "[" + message[0] + "]: " + message[1] : message[0] : string.Empty);
+							if (message.Length > 0) {
+								print(message.Length > 1 ? assemblyName.Name + " -> " + message[1] : assemblyName.Name + " -> " + message[0]);
+							}
+						}
+					}
+					messages.Clear();
+				}
+			}
+		}
 
-        private void LateUpdate()
-        {
-            Flush();
-        }
+		private void LateUpdate() {
+			Flush();
+		}
 
-        #endregion
+		#endregion
 
-        #region Destruction
+		#region Destruction
 
-        private void OnDestroy()
-        {
-            Flush();
-        }
+		private void OnDestroy() {
+			Flush();
+		}
 
-        ~Logger()
-        {
-            Flush();
-        }
+		~Logger() {
+			Flush();
+		}
 
-        #endregion
-    }
+		#endregion
+	}
 }
--- PlanetShine/Utils/Utils.cs
+++ PlanetShine/Utils/Utils.cs
@@ -14,95 +14,84 @@ using System;
 using System.IO;
 using UnityEngine;
 
-namespace PlanetShine
-{
-    public class Utils
-    {
-        public static LineRenderer CreateDebugLine(Color startColor, Color endColor)
-        {
-            GameObject obj = new GameObject ("Line");
-            LineRenderer line = obj.AddComponent< LineRenderer > ();
-            line.material = new Material (Shader.Find ("Particles/Additive"));
-            line.SetColors (startColor, endColor);
-            line.SetWidth (0.05f, 0.05f); 
-            line.SetVertexCount (2);
-            return line;
-        }
+namespace PlanetShine {
+	public class Utils {
+		public static LineRenderer CreateDebugLine(Color startColor, Color endColor) {
+			GameObject obj = new GameObject("Line");
+			LineRenderer line = obj.AddComponent<LineRenderer>();
+			line.material = new Material(Shader.Find("Particles/Additive"));
+			line.positionCount = 2;
+			line.endColor = endColor;
+			line.startColor = startColor;
+			line.startWidth = 0.05f;
+			line.endWidth = 0.05f;
+			return line;
+		}
 
-        public static Color GetUnreadableTextureAverageColor(Texture2D texture)
-        {
-            Texture2D readableTexture = CreateReadable(texture);
-            Color color = GetTextureAverageColor(readableTexture);
-            UnityEngine.Object.Destroy(readableTexture);
-            return color;
-        }
+		public static Color GetUnreadableTextureAverageColor(Texture2D texture) {
+			Texture2D readableTexture = CreateReadable(texture);
+			Color color = GetTextureAverageColor(readableTexture);
+			UnityEngine.Object.Destroy(readableTexture);
+			return color;
+		}
 
-        public static Color GetPixelsAverageColor(Color[] texColors)
-        {
-            int total = texColors.Length;
+		public static Color GetPixelsAverageColor(Color[] texColors) {
+			int total = texColors.Length;
 
-            float r = 0;
-            float g = 0;
-            float b = 0;
+			float r = 0;
+			float g = 0;
+			float b = 0;
 
-            foreach (Color pixel in texColors)
-            {
-                r += pixel.r;
-                g += pixel.g;
-                b += pixel.b;
-            }
-            return new Color(r / total, g / total, b / total, 1.0f);
-        }
+			foreach (Color pixel in texColors) {
+				r += pixel.r;
+				g += pixel.g;
+				b += pixel.b;
+			}
+			return new Color(r / total, g / total, b / total, 1.0f);
+		}
 
-        public static Color GetTextureAverageColor(Texture2D texture)
-        {
-            return GetPixelsAverageColor(texture.GetPixels());
-        }
+		public static Color GetTextureAverageColor(Texture2D texture) {
+			return GetPixelsAverageColor(texture.GetPixels());
+		}
 
-        public static Color GetRimOuterColor(Texture2D texture, float fraction)
-        {
-            return GetPixelsAverageColor(texture.GetPixels(0, 0, (int) Math.Round(texture.width * fraction), texture.height));
-        }
+		public static Color GetRimOuterColor(Texture2D texture, float fraction) {
+			return GetPixelsAverageColor(texture.GetPixels(0, 0, (int)Math.Round(texture.width * fraction), texture.height));
+		}
 
-        public static Texture2D CreateReadable(Texture2D original)
-        {
-            // Checks
-            if (original == null) return null;
-            if (original.width == 0 || original.height == 0) return null;
+		public static Texture2D CreateReadable(Texture2D original) {
+			// Checks
+			if (original == null) return null;
+			if (original.width == 0 || original.height == 0) return null;
 
-            // Create the new texture
-            Texture2D finalTexture = new Texture2D(original.width, original.height);
+			// Create the new texture
+			Texture2D finalTexture = new Texture2D(original.width, original.height);
 
-            // isn't read or writeable ... we'll have to get tricksy
-            RenderTexture rt = RenderTexture.GetTemporary(original.width, original.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB, 1);
-            Graphics.Blit(original, rt);
-            RenderTexture.active = rt;
+			// isn't read or writeable ... we'll have to get tricksy
+			RenderTexture rt = RenderTexture.GetTemporary(original.width, original.height, 0, RenderTextureFormat.ARGB32, RenderTextureReadWrite.sRGB, 1);
+			Graphics.Blit(original, rt);
+			RenderTexture.active = rt;
 
-            // Load new texture
-            finalTexture.ReadPixels(new Rect(0, 0, finalTexture.width, finalTexture.height), 0, 0);
+			// Load new texture
+			finalTexture.ReadPixels(new Rect(0, 0, finalTexture.width, finalTexture.height), 0, 0);
 
-            // Kill the old one
-            RenderTexture.active = null;
-            RenderTexture.ReleaseTemporary(rt);
+			// Kill the old one
+			RenderTexture.active = null;
+			RenderTexture.ReleaseTemporary(rt);
 
-            // Return
-            return finalTexture;
-        }
-    }
+			// Return
+			return finalTexture;
+		}
+	}
 
 
-    public class DisplaySettingOption<T>
-    {
-        public string label { get; private set; }
-        public T value { get; private set; }
+	public class DisplaySettingOption<T> {
+		public string label { get; private set; }
+		public T value { get; private set; }
 
-        public DisplaySettingOption(string label, T value)
-        {
-            this.label = label;
-            this.value = value;
-        }
-    }
-
-
+		public DisplaySettingOption(string label, T value) {
+			this.label = label;
+			this.value = value;
+		}
+	}
 }
 
